c6798a46e9900681526ba5c4dd0e36e449b8de8c
VimIcewind <VimIcewind@gmail.com>
2024-03-20 14:45:18 +0800

    Fix bug: java.lang.Exception: No runnable methods

--- src/test/java/com/vimemacs/test/BaseJunit4Test.java
+++ src/test/java/com/vimemacs/test/BaseJunit4Test.java
@@ -1,5 +1,6 @@
 package com.vimemacs.test;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -11,5 +12,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations={"classpath:applicationContext.xml"})
 public class BaseJunit4Test {
-    
+    @Test
+    public void testNone() {
+    }
 }
