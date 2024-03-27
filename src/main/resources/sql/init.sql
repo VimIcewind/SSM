1d02c79f84d6815f5336b815046ef26527e5abdd
VimIcewind <VimIcewind@gmail.com>
2024-03-27 11:56:07 +0800

    Just format code style

--- src/main/resources/sql/init.sql
+++ src/main/resources/sql/init.sql
@@ -3,10 +3,10 @@
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
-                        `id` bigint(32) NOT NULL AUTO_INCREMENT,
-                        `name` varchar(10) DEFAULT NULL,
-                        `password` varchar(64) DEFAULT NULL,
-                        PRIMARY KEY (`id`)
+    `id` bigint(32) NOT NULL AUTO_INCREMENT,
+    `name` varchar(10) DEFAULT NULL,
+    `password` varchar(64) DEFAULT NULL,
+    PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
