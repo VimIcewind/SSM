fa6f8b72d94f5d7586b348afe96db1848a2882da
VimIcewind <VimIcewind@gmail.com>
2024-07-02 15:57:46 +0800

    MyBatis @Select

--- src/main/java/com/vimemacs/dao/UserDao.java
+++ src/main/java/com/vimemacs/dao/UserDao.java
@@ -2,6 +2,9 @@ package com.vimemacs.dao;
 
 import com.vimemacs.model.User;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,12 +15,20 @@ import java.util.List;
  */
 @Repository
 public interface UserDao {
+    @Select("select * from user")
+    @Results({
+            @Result(id = true, column = "id", property = "id"),
+            @Result(column = "name", property = "name"),
+            @Result(column = "password", property = "password")
+    })
     List<User> findAll();
     
+    @Select("select * from user where id = #{id}")
     User findById(String id);
 
     List<User> findByIds(@Param("list") List<Integer> list);
     
+    @Select("select * from user where name = #{name}")
     List<User> findByName(@Param("name") String name);
 
     List<User> findByNameAndPassword(@Param("name") String name, @Param("password") String password);
